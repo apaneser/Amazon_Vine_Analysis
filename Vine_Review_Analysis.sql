ded793a515572f97c3b4e52346fc0ae3112ae9c8
apaneser <56094764+apaneser@users.noreply.github.com>
2021-10-17 03:11:25 -0700

    D2 part 2

---
+++ Vine_Review_Analysis.sql
@@ -0,0 +1,10 @@
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
+
+SELECT * FROM vine_table;
